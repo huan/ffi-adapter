71e8d776cc9da6527c7f48535cbf0145919c573e
Huan LI <zixia@zixia.net>
2020-02-04 19:28:13 +0800

    fix ci

--- scripts/build-dll.bat
+++ scripts/build-dll.bat
@@ -1,3 +1,5 @@
+REM https://travis-ci.community/t/how-to-c-in-windows/3273/4?u=huan
+
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -test
 
