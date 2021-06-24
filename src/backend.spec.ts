ab28bd1590ea74717bb123e88019d784eaa68d6b
Huan (李卓桓) <zixia@zixia.net>
2021-06-24 22:02:17 +0800

    add backend singleton test

--- src/backend.spec.ts
+++ src/backend.spec.ts
@@ -3,6 +3,14 @@
 // tslint:disable:no-shadowed-variable
 import test  from 'blue-tape'
 
+import { Backend } from './backend'
+
 test('Class & Properties decorate should orderd one by one', async t => {
   t.skip('to be write')
+})
+
+test('Singleton: new twice will get the same instance', async t => {
+  const b1 = new Backend()
+  const b2 = new Backend()
+  t.equal(b1, b2, 'should get singleton instances')
 })
