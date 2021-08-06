6dd1da0fae0f635ba443f70045b58a114f944a2f
Huan <zixia@zixia.net>
2021-08-06 12:18:59 +0800

    add arm64 support

--- tests/fixtures/library/index.ts
+++ tests/fixtures/library/index.ts
@@ -1,10 +1,6 @@
 import path from 'path'
 
-const libfactorialFile = process.arch === 'x64'
-  ? 'libfactorial-x64'
-  : 'libfactorial-x86'
-
 export const FIXTURE_LIB_FACTORIAL_FILE = path.join(
   __dirname,
-  libfactorialFile,
+  `libfactorial-${process.arch}`,
 )
